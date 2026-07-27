7f700033fbcc0bbc77c26c393675c475a99fa84e
Florian Reisinger <florian.reisinger.at@gmail.com>
2026-07-27 18:08:19 +0200

    fix(demo): cropper route to root path /

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: '/cropper', pathMatch: 'full' },
-  { path: 'cropper', loadComponent: () => import('./pages/cropper-demo/cropper-demo').then(m => m.CropperDemo) },
+  { path: '', loadComponent: () => import('./pages/cropper-demo/cropper-demo').then(m => m.CropperDemo) },
 ];
